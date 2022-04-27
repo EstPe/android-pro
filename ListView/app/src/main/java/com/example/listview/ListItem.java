a8638d3c56317c4a9cd740dfb834f7fc5d87866d
EstPe <peretz398@gmail.com>
2022-04-27 21:08:34 +0300

    upload login and singup

---
+++ ListView/app/src/main/java/com/example/listview/ListItem.java
@@ -0,0 +1,12 @@
+package com.example.listview;
+
+public class ListItem {
+    public String name;
+    public String Desc;
+    public ListItem(String name,String Desc)
+    {
+        this.name=name;
+        this.Desc=Desc;
+    }
+
+}
